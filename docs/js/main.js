5492ce46fee96b943a98a3b59580b7ca8475f54a
lolarg <alanplaza1@gmail.com>
2023-11-14 03:02:36 -0300

    x

---
+++ docs/js/main.js
@@ -0,0 +1,17 @@
+// Bootstrap Tooltip init script
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+jQuery('.nav-item').on('click',function(){
+  jQuery(this).children().addClass('active');
+  jQuery(this).siblings().children().removeClass('active');
+})
